e38f3c9cc6846dd55c97e3f1630395c05a0a6964
Noah Loomans <noahloomans@gmail.com>
2018-01-23 14:11:15 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'metis_walldisplay',
+  packages = ['metis_walldisplay'],
+  version = '0.1',
+  description = 'Communicate using serial to the wall display we have at metis',
+  author = 'Noah Loomans',
+  author_email = 'noahloomans@gmail.com',
+  url = 'https://github.com/nloomans/metis_walldisplay',
+  download_url = 'https://github.com/nloomans/metis_walldisplay/archive/0.1.tar.gz',
+  keywords = [],
+  classifiers = [],
+)
