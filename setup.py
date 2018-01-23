c4cf2fa9ad994a8405092f8cf81c786cfbf4215a
Noah Loomans <noahloomans@gmail.com>
2018-01-23 14:48:03 +0100

    Fix pip setup

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(
-  name = 'metis_walldisplay',
-  packages = ['metis_walldisplay'],
-  version = '0.1',
-  description = 'Communicate using serial to the wall display we have at metis',
-  author = 'Noah Loomans',
-  author_email = 'noahloomans@gmail.com',
-  url = 'https://github.com/nloomans/metis_walldisplay',
-  download_url = 'https://github.com/nloomans/metis_walldisplay/archive/0.1.tar.gz',
-  keywords = [],
-  classifiers = [],
+    name='metis_walldisplay',
+    version='0.0.1',
+    description='Communicate using serial to the wall display we have at metis',
+    url='https://github.com/nloomans/metis_walldisplay',
+    author='Noah Loomans',
+    py_modules=["metis_walldisplay"],
 )
